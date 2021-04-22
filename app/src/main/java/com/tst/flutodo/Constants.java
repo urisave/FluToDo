fbce36a3c9c27735452909124b8d0a52477b721f
Oriol <oriol.santacana@gmail.com>
2021-04-23 00:22:28 +0200

    Strings constants.

--- app/src/main/java/com/tst/flutodo/Constants.java
+++ app/src/main/java/com/tst/flutodo/Constants.java
@@ -3,4 +3,5 @@ package com.tst.flutodo;
 public class Constants {
 
     public static final String URL_API_LOCAL = "http://localhost:8080/api/todo/";
+    public static final String MSG_EMPTY_TASK = "Task name is empty";
 }
