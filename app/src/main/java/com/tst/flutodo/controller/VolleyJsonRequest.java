ed89cd7404d7cba0bbb230480a2ea25680b7cadc
Oriol <oriol.santacana@gmail.com>
2021-04-25 01:33:28 +0200

    Added subclass to manage response empty with status code 200 to be align with API responses.

---
+++ app/src/main/java/com/tst/flutodo/controller/VolleyJsonRequest.java
@@ -0,0 +1,31 @@
+package com.tst.flutodo.controller;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public class VolleyJsonRequest extends JsonObjectRequest {
+
+    public VolleyJsonRequest(int method, String url, @Nullable JSONObject jsonRequest, Response.Listener<JSONObject> listener, @Nullable Response.ErrorListener errorListener) {
+        super(method, url, jsonRequest, listener, errorListener);
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            if (response.statusCode == 200) {
+                byte[] responseData = "{}".getBytes("UTF8");
+                response = new NetworkResponse(response.statusCode, responseData, response.headers, response.notModified);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return super.parseNetworkResponse(response);
+    }
+}
